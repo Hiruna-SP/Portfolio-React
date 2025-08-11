7fab02635ff0ae7de0b6c29df9f84b872ba7b5a2
Hiruna-SP <hirunasp@gmail.com>
2025-08-11 15:20:21 +0530

    added project images to src and update the projects_data.js

--- src/assets/projects_data.js
+++ src/assets/projects_data.js
@@ -1,33 +1,40 @@
+import img1 from "../assets/cal.png";
+import img2 from "../assets/agriadpt.jpg";
+import img3 from "../assets/ceylon bliss international.png";
+import img4 from "../assets/finance_tracker.png";
+import img5 from "../assets/student management system.png";
+import img6 from "../assets/portfolio.png";
+
 const projects_data = [
   {
     p_no: 1,
     p_name: "Project One",
-    p_img: "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    p_img: img1,
   },
   {
     p_no: 2,
     p_name: "Project Two",
-    p_img: "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    p_img: img2,
   },
   {
     p_no: 3,
     p_name: "Project Three",
-    p_img: "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    p_img: img3,
   },
   {
     p_no: 4,
     p_name: "Project Four",
-    p_img: "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    p_img: img4,
   },
   {
     p_no: 5,
     p_name: "Project Five",
-    p_img: "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    p_img: img5,
   },
   {
     p_no: 6,
     p_name: "Project Six",
-    p_img: "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI",
+    p_img: img6,
   },
 ];
 
